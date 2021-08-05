a40a642790b3d36f4031a7d13ca085fb98a94b10
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-05 17:28:21 +0900

    Update problem_62.py

--- 박제윤/interviews/chapter_17/problem_62.py
+++ 박제윤/interviews/chapter_17/problem_62.py
@@ -1,1 +1,14 @@
-pass
+# page: p507
+# sol) sort and compare
+def is_anagram(s: str, t: str) -> bool:
+  return sorted(s) == sorted(t)
+
+# true
+s = "anagram"
+t = "nagaram"
+
+# false
+# s = "rat"
+# t = "car"
+
+print(is_anagram(s, t))
