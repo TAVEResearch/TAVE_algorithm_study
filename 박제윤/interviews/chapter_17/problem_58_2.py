889bc0d48be4cc83b2659872548742b358b7d23c
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-05 16:10:27 +0900

    Update problem_58_2.py

--- 박제윤/interviews/chapter_17/problem_58_2.py
+++ 박제윤/interviews/chapter_17/problem_58_2.py
@@ -1,3 +1,10 @@
 # page: p493
 # sol.2) Quick sort, no Lomuto (time-out issue)
+
+"""
+It should be noted that Quick sort is not appropriate for Linked list:
+
+1) It is difficult to set the pivot to the desired shape
+2) If it is already sorted, it continues to be divided into an unbalanced list.
+"""
 pass
