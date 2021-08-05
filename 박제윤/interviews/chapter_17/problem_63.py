46bb77a618faae0924e498582b273c31bc2d63fd
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-05 17:43:07 +0900

    Update problem_63.py

--- 박제윤/interviews/chapter_17/problem_63.py
+++ 박제윤/interviews/chapter_17/problem_63.py
@@ -1,1 +1,22 @@
-pass
+# page: p508
+# sol) Using Dutch National Flag Problem
+from typing import List
+
+def sortColors(nums: List[int]) -> None:
+  red, white, blue = 0, 0, len(nums)
+
+  while white < blue:
+    if nums[white] < 1:
+      nums[red], nums[white] = nums[white], nums[red]
+      white += 1
+      red += 1
+    elif nums[white] > 1:
+      blue -= 1
+      nums[white], nums[blue] = nums[blue], nums[white]
+    else:
+      white +=1
+
+# output = [0, 0, 1, 1, 2, 2]
+nums = [2, 0, 2, 1, 1, 0]
+sortColors(nums)
+print(nums)
