e00f8080d2388e8bb9af6a9d0036dc3ff61ee01f
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-05 16:23:47 +0900

    Update problem_59.py

--- 박제윤/interviews/chapter_17/problem_59.py
+++ 박제윤/interviews/chapter_17/problem_59.py
@@ -1,3 +1,17 @@
 # page: 497p
 # sol) sort and merge
-pass
+from typing import List
+
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+  merged = []
+
+  for i in sorted(intervals, key = lambda x: x[0]):
+    if merged and i[0] <= merged[-1][1]:
+      merged[-1][1] = max(merged[-1][1], i[1])
+    else:
+      merged += i,
+
+  return merged
+
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge(intervals))
