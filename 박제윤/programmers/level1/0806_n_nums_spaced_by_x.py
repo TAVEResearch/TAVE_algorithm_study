86ade68de31e67a00dbbb917f82ccc8474ab5bb3
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-06 16:42:36 +0900

    Update 0806_n_nums_spaced_by_x.py

--- 박제윤/programmers/level1/0806_n_nums_spaced_by_x.py
+++ 박제윤/programmers/level1/0806_n_nums_spaced_by_x.py
@@ -1,11 +1,4 @@
 from typing import List
 
 def solution(x: int, n: int) -> List[int]:
-    answer = []
-    increase = x
-
-    for _ in range(n):
-      answer.append(x)
-      x = x + increase
-
-    return answer
+    return [ x + x * dist for dist in range(n)]
