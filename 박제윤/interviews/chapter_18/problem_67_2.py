eec9295aeb6ee0d3b779d3acc1aae5499151b7fe
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 15:34:42 +0900

    Update problem_67_2.py

--- 박제윤/interviews/chapter_18/problem_67_2.py
+++ 박제윤/interviews/chapter_18/problem_67_2.py
@@ -1,1 +1,29 @@
-pass
+# page: p530
+# sol.2) Binary Search
+# O(NlogN)
+from typing import List, Set
+import bisect
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+  result: Set = set()
+
+  nums2.sort()
+
+  for n1 in nums1:
+    # binary search
+    i2 = bisect.bisect_left(nums2, n1)
+
+    if len(nums2) > 0 and len(nums2) >i2 and n1 == nums2[i2]:
+      result.add(n1)
+
+  return list(result)
+
+# result = [2]
+# nums1 = [1, 2, 2, 1]
+# nums2 = [2, 2]
+
+# result = [9, 4]
+nums1 = [4, 9, 5]
+nums2 = [9, 4, 9, 8, 4] 
+
+print(intersection(nums1, nums2))
