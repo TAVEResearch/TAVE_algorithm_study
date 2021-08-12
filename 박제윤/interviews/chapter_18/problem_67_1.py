aaa2241fe330bb033a6ab2124d52cc7d79d4d320
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 15:28:02 +0900

    Update problem_67_1.py

--- 박제윤/interviews/chapter_18/problem_67_1.py
+++ 박제윤/interviews/chapter_18/problem_67_1.py
@@ -1,1 +1,23 @@
-pass
+# page: p529
+# sol.1) Brute Force
+from typing import List, Set
+
+def intersection(nums1: List[int], nums2: List[int]) -> Set[int]:
+  result: Set = set()
+
+  for n1 in nums1:
+    for n2 in nums2:
+      if n1 == n2:
+        result.add(n1)
+
+  return result
+
+# result = [2]
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+
+# result = [9, 4]
+# nums1 = [4, 9, 5]
+# nums2 = [9, 4, 9, 8, 4] 
+
+print(intersection(nums1, nums2))
