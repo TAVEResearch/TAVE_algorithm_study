499ac193c043f152fce6e68f015a4dae0f3468a6
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 10:30:11 +0900

    Update problem_65_1.py

--- 박제윤/interviews/chapter_18/problem_65_1.py
+++ 박제윤/interviews/chapter_18/problem_65_1.py
@@ -1,22 +1,24 @@
 # page: p518
 # sol.1) recursion
+
+# It should be noted that the limitation of recursion call is 1,000
 from typing import List
 
 def search(nums: List[int], target: int) -> int:
-  def binary_search(left, right):
+  def binary_search(left: int, right: int) -> int:
     if left <= right:
       mid = (left + right) // 2
 
       if nums[mid] < target:
         return binary_search(mid + 1, right)
       elif nums[mid] > target:
-        return binary_search(left, mid - 1)
-      else:
+        return binary_search(left, mid - 1)  
+      else: # nums[mid] == target
         return mid
-    else:
+    else: # error
       return -1
 
-  return binary_search(0, len(nums) - 1)
+  return binary_search(0, len(nums) - 1) # left, right
 
 nums = [-1, 0, 3, 5, 9 ,12] # sorted
 target = 9
