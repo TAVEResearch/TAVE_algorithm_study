276d2f42a9532060f5a60c02a552d15dd723a804
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 11:15:29 +0900

    Update problem_65_4.py

--- 박제윤/interviews/chapter_18/problem_65_4.py
+++ 박제윤/interviews/chapter_18/problem_65_4.py
@@ -1,1 +1,17 @@
-pass
+# page: p520
+# sol.4) index()
+
+from typing import List
+import bisect
+
+def search(nums: List[int], target: int) -> int:
+  try:
+    return nums.index(target)
+  except ValueError:
+    return -1
+
+
+nums = [-1, 0, 3, 5, 9 ,12] # sorted
+target = 9
+
+print(search(nums, target))
