511ddab9c60457403f724aea10b56ebf3854cbd4
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 14:17:09 +0900

    Update and rename problem_66_0_pivot.py to problem_66_1.py

---
+++ 박제윤/interviews/chapter_18/problem_66_1.py
@@ -0,0 +1,44 @@
+# page: p525
+# sol.1) Binary search
+
+# No Numpy!
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+  # exception
+  if not nums:
+    return -1
+  
+  # find pivot
+  left, right = 0, len(nums) - 1
+  while left < right:
+    mid = left + (right - left) // 2
+    
+    if nums[mid] > nums[right]:
+      left = mid + 1
+    else:
+      right = mid
+  
+  pivot = left
+  
+  # binary search based on the pivot and Repearting 
+  left, right = 0, len(nums) - 1
+  
+  while left <= right:
+    mid = left + (right - left) // 2
+    mid_pivot = (mid + pivot) % len(nums)
+
+    if nums[mid_pivot] <target:
+      left = mid + 1
+    elif nums[mid_pivot] > target:
+      right = mid + 1
+    else:
+      return mid_pivot
+
+  return -1
+
+# result = 5
+nums = [4, 5, 6, 7, 0, 1, 2] # partially sorted
+target = 1
+
+print(search(nums, target))
