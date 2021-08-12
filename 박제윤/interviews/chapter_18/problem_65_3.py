cc8006a0e3cd46677b958e3cc7b865222b6271d0
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 11:10:05 +0900

    Update problem_65_3.py

--- 박제윤/interviews/chapter_18/problem_65_3.py
+++ 박제윤/interviews/chapter_18/problem_65_3.py
@@ -1,1 +1,19 @@
-pass
+# page: p520
+# sol.3) bisect module
+
+from typing import List
+import bisect
+
+def search(nums: List[int], target: int) -> int:
+  index  = bisect.bisect_left(nums, target)
+
+  if index < len(nums) and nums[index] == target:
+    return index
+  else:
+    return -1
+
+
+nums = [-1, 0, 3, 5, 9 ,12] # sorted
+target = 9
+
+print(search(nums, target))
