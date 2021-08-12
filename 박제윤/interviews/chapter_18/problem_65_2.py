4914c8bd3fed4ff01799cbc2508d872b08450301
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 11:04:27 +0900

    Update problem_65_2.py

--- 박제윤/interviews/chapter_18/problem_65_2.py
+++ 박제윤/interviews/chapter_18/problem_65_2.py
@@ -1,1 +1,26 @@
-pass
+# page: p518
+# sol.2) repeating
+
+# Recursion -> repeating: simple!
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+  left, right = 0, len(nums) - 1
+
+  while left <= right:
+    mid = (left + right) // 2
+
+    if nums[mid] < target:
+      left = mid + 1
+    elif nums[mid] > target:
+      right mid - 1
+    else:
+      return mid
+  
+  return -1
+
+
+nums = [-1, 0, 3, 5, 9 ,12] # sorted
+target = 9
+
+print(search(nums, target))
