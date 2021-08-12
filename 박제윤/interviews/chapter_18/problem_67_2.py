9de39c053df088d61a6026bb9304d97fb4e01d8f
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-12 15:47:14 +0900

    Update problem_67_2.py

--- 박제윤/interviews/chapter_18/problem_67_2.py
+++ 박제윤/interviews/chapter_18/problem_67_2.py
@@ -13,7 +13,7 @@ def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
     # binary search
     i2 = bisect.bisect_left(nums2, n1)
 
-    if len(nums2) > 0 and len(nums2) >i2 and n1 == nums2[i2]:
+    if len(nums2) > 0 and len(nums2) > i2 and n1 == nums2[i2]:
       result.add(n1)
 
   return list(result)
