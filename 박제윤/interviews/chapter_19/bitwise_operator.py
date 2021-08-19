fe5c11dc02487e67379b52d96d58331f4cc220aa
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-19 15:03:18 +0900

    Update bitwise_operator.py

--- 박제윤/interviews/chapter_19/bitwise_operator.py
+++ 박제윤/interviews/chapter_19/bitwise_operator.py
@@ -8,3 +8,7 @@ print(True ^ True)
 # e.g.) -12 = Not 12 + 1
 # So, "~": Not x = -x - 1 
 print(~ True)
+
+# 1) ~0b1100 -> 0b0011 -> 0b0010
+# 2) -ob1100 -> bin(0b1100 ^ 0b1111) : using MASK
+print(bin(0b0101 ^ ~0b1100))
