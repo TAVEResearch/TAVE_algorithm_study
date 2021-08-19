79da0323afe5690363e8f86e5f542d628240a4bc
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-19 11:34:04 +0900

    Create 0819_matrix_sum.py

---
+++ 박제윤/programmers/level1/0819_matrix_sum.py
@@ -0,0 +1,17 @@
+# date: 2021.08.19
+# author: jeiyoon
+from typing import List
+
+# Two matrices with equal rows and columns
+def solution(arr1: List[List[int]], 
+             arr2: List[List[int]]) -> List[List[int]]:
+    
+    answer = []
+    
+    for a1, a2 in zip(arr1, arr2):
+      print(a1, a2) 
+      result = [a1_i + a2_i for a1_i, a2_i in zip(a1,a2)]
+    
+      answer.append(result)
+      
+    return answer
