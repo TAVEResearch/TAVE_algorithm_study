1184ae1007a6669c71454748461fa24956aa22e3
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-19 15:30:07 +0900

    Update problem_70.py

--- 박제윤/interviews/chapter_19/problem_70.py
+++ 박제윤/interviews/chapter_19/problem_70.py
@@ -1,1 +1,19 @@
-pass
+# page: p552
+# sol) XOR
+from typing import List
+
+def singleNumber(nums: List[int]) -> int:
+  result = 0
+
+  for num in nums:
+    result ^= num
+  
+  return result
+
+# result = 1
+nums = [2, 2, 1]
+
+# result = 4
+# nums = [4, 1, 2, 1, 2]
+
+print(singleNumber(nums))
