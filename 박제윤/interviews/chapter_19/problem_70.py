854ae133fa9258dcd458709b9af0dea810804ab3
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-19 16:11:13 +0900

    Update problem_70.py

--- 박제윤/interviews/chapter_19/problem_70.py
+++ 박제윤/interviews/chapter_19/problem_70.py
@@ -4,16 +4,26 @@ from typing import List
 
 def singleNumber(nums: List[int]) -> int:
   result = 0
+  print(nums)
 
   for num in nums:
+    print("(before) result: ", result)
+    print("num: ", num)
     result ^= num
-  
+    print("result = result ^ num")
+    print("(after) result: ", result)
+    print(bin(result))    
+    print(" ")
+
   return result
 
 # result = 1
-nums = [2, 2, 1]
+# nums = [2, 2, 1]
 
 # result = 4
-# nums = [4, 1, 2, 1, 2]
+nums = [4, 1, 2, 1, 2]
+# nums = [1, 1, 2, 2, 4]
+# nums = [1,2,1,2]
 
+# e.g.) if a = 6, b = 3 -> a ^ b = 5 (110 ^ 011 => 101)
 print(singleNumber(nums))
