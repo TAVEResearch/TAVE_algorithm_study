92af295028fa43c581b753a3bcdeecce5728ee95
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-19 15:25:02 +0900

    Update bitwise_operator.py

--- 박제윤/interviews/chapter_19/bitwise_operator.py
+++ 박제윤/interviews/chapter_19/bitwise_operator.py
@@ -12,3 +12,19 @@ print(~ True)
 # 1) ~0b1100 -> 0b0011 -> 0b0010
 # 2) -ob1100 -> bin(0b1100 ^ 0b1111) : using MASK
 print(bin(0b0101 ^ ~0b1100))
+
+print(bin(87))
+print(int('1010111', 2))
+
+a = bin(87)
+print(type(a))
+
+b = 0b1010111
+print(type(b))
+
+c = 0x57 # hex(87)
+print(c)
+
+print(id(87))
+print(id(b))
+print(id(c))
