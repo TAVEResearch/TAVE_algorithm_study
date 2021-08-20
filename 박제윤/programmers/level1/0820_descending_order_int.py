4bd22f2f2177700faebc69fba202e77bf4a7c52e
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-20 16:49:07 +0900

    Update 0820_descending_order_int.py

--- 박제윤/programmers/level1/0820_descending_order_int.py
+++ 박제윤/programmers/level1/0820_descending_order_int.py
@@ -1,4 +1,5 @@
-
+# date: 2021.08.20
+# author: jeiyoon
 def solution(num: int) -> int:
   # int -> str -> list -> reverse sorting -> str -> int
   return int("".join(sorted(list(str(num)), reverse = True)))
