9f03d7191aa6fc8d6c047a5478e909ea930a4e7f
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-23 10:44:37 +0900

    Update 0823_str_compression.py

--- 박제윤/programmers/level2/0823_str_compression.py
+++ 박제윤/programmers/level2/0823_str_compression.py
@@ -4,13 +4,14 @@
 def solution(s: str) -> int:
     min_length = len(s)
 
-    for window_size in range(1, int(len(s)/2) + 1) : # the size of sliding window
-      
+    for window_size in range(1, int(len(s)/2) + 1) : # the size of sliding window   
       coefficient = 1
       result = []
+        
       for idx in range(0, len(s) - window_size, window_size):
         if s[idx:window_size + idx] == s[idx + window_size:window_size + idx + window_size]: # same
           coefficient += 1
+        
           if idx >= len(s) - window_size - window_size: # last 
             result.append(str(coefficient) + s[idx:window_size + idx])
         else: # different
