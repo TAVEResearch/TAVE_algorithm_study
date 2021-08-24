3238e6f2472b3d75a81620bb58c61bd6662ed87d
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-24 13:44:05 +0900

    Create 0824_world_124.py

---
+++ 박제윤/programmers/level2/0824_world_124.py
@@ -0,0 +1,23 @@
+# date: 2021.08.24
+# author: jeiyoon
+
+"""
+Timeout error occurs and i dont know why
+"""
+from typing import List
+from itertools import product
+
+def solution(n: int) -> int:
+  # 500,000,000
+  # exception
+  for p in range(19): # power is 0 to 18  
+    if n - (3 ** p) >= 0: 
+      n = n - (3 ** p)
+    else:
+      return "".join(list(product(['1', '2', '4'], repeat = p))[n])
+    
+n_list = [i for i in range(1,14)]
+print(n_list)
+for n in n_list:
+  # print("n: ", n)
+  print(solution(n))
