a36570280550422b9a4bdc1df31137c5173dfa70
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-26 20:10:22 +0900

    Update problem_75_2.py

--- 박제윤/interviews/chapter_20/problem_75_2.py
+++ 박제윤/interviews/chapter_20/problem_75_2.py
@@ -1,1 +1,18 @@
-pass
+# p571
+# sol.1) brute force
+from typing import List
+
+def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+  if not nums:
+    return nums
+
+  r = []
+
+  for i in range(len(nums) - k + 1):
+    r.append(max(nums[i:i+k]))
+
+  return r
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7] # 6 = 8 - 3 + 1
+k = 3
+print(maxSlidingWindow(nums, k))
