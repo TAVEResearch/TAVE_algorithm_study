d8211ecfc62a84871a7d0469a5260273d2fa6b3d
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-08-26 20:07:30 +0900

    Update problem_75_1.py

--- 박제윤/interviews/chapter_20/problem_75_1.py
+++ 박제윤/interviews/chapter_20/problem_75_1.py
@@ -1,1 +1,16 @@
-pass
+# page: p571
+# sol.1) brute force
+from typing import List
+
+def solution(nums: List[int], k: int) -> List[int]:
+  answer = []
+  for n in range(len(nums) - k + 1):
+    answer.append(max(nums[n:n+k]))
+
+  return answer
+
+# result = [3, 3, 5, 5, 6, 7]
+nums = [1, 3, -1, -3, 5, 3, 6, 7] # 6 = 8 - 3 + 1
+k = 3
+
+print(solution(nums, k))
