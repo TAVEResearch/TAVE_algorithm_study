f918eab31d778c8fb30ed3f748968b7f9d8868cf
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-11-18 21:00:26 +0900

    Update 1118_2016.py

--- 박제윤/programmers/level1/1118_2016.py
+++ 박제윤/programmers/level1/1118_2016.py
@@ -2,7 +2,6 @@
 # author: jeiyoon
 def solution(a: int, b: int) -> str:
     # 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
-    def solution(month, date):
     if month < 1 or month > 13:
         return -1
     
