2a209c034a1c38f1fadfcdf1940ee44100985561
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2021-11-24 11:08:00 +0900

    Update 1124_the_num_of_p_and_y.py

--- 박제윤/programmers/level1/1124_the_num_of_p_and_y.py
+++ 박제윤/programmers/level1/1124_the_num_of_p_and_y.py
@@ -1,1 +1,13 @@
-pass
+# date: 2021.11.24
+# author: jeiyoon
+def solution(s: str) -> bool:
+  s = s.lower()
+  return s.count('p') == s.count('y')
+
+# answer = true
+# s = "pPoooyY"
+
+# answer = false
+s = "Pyy"
+
+print(solution(s))
