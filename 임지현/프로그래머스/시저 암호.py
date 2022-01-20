4459f3b5e8ed331dabc5ecce7d6d76a0d1133e57
Jihyun Lim <55045377+jadeneu@users.noreply.github.com>
2022-01-20 15:45:48 +0900

    Create 시저 암호.py

---
+++ 임지현/프로그래머스/시저 암호.py
@@ -0,0 +1,19 @@
+def solution(s, n):
+    answer = ''
+    for char in s:
+      
+        # char가 공백일 경우
+        if char == " ":
+            answer += " "
+            continue
+            
+        digit = ord(char)
+        digit += n
+        if char.isupper() and digit > 90:
+            r = digit % 90
+            digit = 64 + r
+        elif char.islower() and digit > 122:
+            r = digit % 122
+            digit = 96 + r
+        answer += chr(digit)
+    return answer
