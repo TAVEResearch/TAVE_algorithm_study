882b745dad3c290f150ac57c32b543978e72104a
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-01-21 16:45:47 +0900

    Update 012122_no_duplication.py

--- 박제윤/programmers/level1/012122_no_duplication.py
+++ 박제윤/programmers/level1/012122_no_duplication.py
@@ -1,3 +1,5 @@
+# date: 2022.01.21
+# author: jeiyoon
 from typing import List
 
 def solution(arr: List[int]) -> List[int]:
@@ -7,7 +9,7 @@ def solution(arr: List[int]) -> List[int]:
     if arr[idx] != arr[idx + 1]:
       result.append(arr[idx])
   
-  result.append(arr[-1])
+  result.append(arr[-1]) # arr[-2] doesn't matter
   return result
 
 # answer = 	[1,3,0,1]
