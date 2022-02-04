dd4b07308de6c23f17a7c381dff6bec74bf30c50
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-02-04 14:53:41 +0900

    Update 020422_menu_renewal.py

--- 박제윤/programmers/level2/020422_menu_renewal.py
+++ 박제윤/programmers/level2/020422_menu_renewal.py
@@ -1,28 +1,39 @@
 # date: 2022.02.03
 # author: jeiyoon
-"""
-orders 배열의 크기는 2 이상 20 이하입니다.
-orders 배열의 각 원소는 크기가 2 이상 10 이하인 문자열입니다.
-각 문자열은 알파벳 대문자로만 이루어져 있습니다.
-각 문자열에는 같은 알파벳이 중복해서 들어있지 않습니다.
-
-course 배열의 크기는 1 이상 10 이하입니다.
-course 배열의 각 원소는 2 이상 10 이하인 자연수가 오름차순으로 정렬되어 있습니다.
-course 배열에는 같은 값이 중복해서 들어있지 않습니다.
-
-정답은 각 코스요리 메뉴의 구성을 문자열 형식으로 배열에 담아 사전 순으로 오름차순 정렬해서 return 해주세요.
-배열의 각 원소에 저장된 문자열 또한 알파벳 오름차순으로 정렬되어야 합니다.
-만약 가장 많이 함께 주문된 메뉴 구성이 여러 개라면, 모두 배열에 담아 return 하면 됩니다.
-orders와 course 매개변수는 return 하는 배열의 길이가 1 이상이 되도록 주어집니다.
-"""
+import sys
 from typing import List
-from collections import Counter, deque
+from collections import Counter
+from itertools import combinations
 
 def solution(orders: List[str], course: List[int]) -> List[str]:
   answer = []
+  course_menu = Counter()
+  max_count = -sys.maxsize
 
+  for course_size in course:
+    for order in orders:
+      course_menu += Counter(combinations(sorted(order), course_size))
 
-  return answer
+    for menu, count in course_menu.most_common():
+      """
+      (1) max count
+      """
+      if count >= max_count and count != 1:
+        max_count = count
+      
+      """
+      (2) check duplication
+      """
+      if count == max_count:
+        answer.append(''.join(menu))
+    
+    """
+    (3) reinit
+    """
+    course_menu = Counter([])
+    max_count = -sys.maxsize
+  
+  return sorted(answer)
 
 # 각 손님들이 주문한 단품메뉴
 orders_list = [["ABCFG", 
@@ -45,4 +56,4 @@ course_list = [[2,3,4],
 # 사전 순으로 오름차순 정렬해서
 for orders, course in zip(orders_list, course_list):
   print(solution(orders, course))
-  break
+  print(" ")
