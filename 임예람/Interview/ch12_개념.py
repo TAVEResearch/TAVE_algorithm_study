bb1362090a59dd512845386d12a06f482c475e14
yeram-lim <yeramyeye@gmail.com>
2022-03-04 15:32:23 +0900

    [interview] ch12

---
+++ 임예람/Interview/ch12_개념.py
@@ -0,0 +1,63 @@
+#https://dndi117.tistory.com/entry/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EA%B7%B8%EB%9E%98%ED%94%84-%EA%B7%B8%EB%9E%98%ED%94%84-%ED%83%90%EC%83%89BFSDFS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EC%9D%98-%EA%B8%B0%EC%B4%88#recentComments
+n, m = 3, 5
+
+#만약 n개만큼의 정점이 존재하고, m개만큼의 입력을 받는다면 
+graph0 = [[] for _ in range(n+1)] # n+1개만큼의 공간을 만들어서 graph[n]이 n번 정점을 나타내도록 해 준다 
+
+# for _ in range(m):
+#     x,y = map(int, input().split()) #만약 1 2를 입력받으면 
+#     graph[x].append(y) # 1번 정점에 2를 넣어주고 -> graph[1] = [2] 
+#     graph[y].append(x) # 2번 정점에 1을 넣어준다 -> graph[2] = [1]
+# print(graph)
+
+graph = {
+    1 : [2,3,4], 
+    2 : [5], 
+    3 : [5], 
+    4 : [], 
+    5 : [6, 7], 
+    6 : [], 
+    7 : [3]
+}
+
+def recursive_dfs(v, discovered=[]):
+    discovered.append(v)
+    print(discoverd)
+    for w in graph[v]:
+        if not w in discoverd:
+            discoverd = recursive_dfs(w, discovered)
+    return discovered
+
+# [1]
+# [1, 2]
+# [1, 2, 5]
+# [1, 2, 5, 6]
+# [1, 2, 5, 6, 7]
+# [1, 2, 5, 6, 7, 3]
+# [1, 2, 5, 6, 7, 3, 4]
+# [1, 2, 5, 6, 7, 3, 4]
+
+def iterative_bfs(start_v):
+    discovered = [start_v]
+    queue = [start_v]
+    while queue:
+        print(discovered, queue)
+        v = queue.pop(0)
+        for w in graph[v]:
+            if w not in discovered:
+                discovered.append(w)
+                queue.append(w)
+    return discovered
+
+# [1] [1]
+# [1, 2, 3, 4] [2, 3, 4]
+# [1, 2, 3, 4, 5] [3, 4, 5]
+# [1, 2, 3, 4, 5] [4, 5]
+# [1, 2, 3, 4, 5] [5]
+# [1, 2, 3, 4, 5, 6, 7] [6, 7]
+# [1, 2, 3, 4, 5, 6, 7] [7]
+# [1, 2, 3, 4, 5, 6, 7]
+
+print(recursive_dfs(1))
+
+# print(graph2[1])
