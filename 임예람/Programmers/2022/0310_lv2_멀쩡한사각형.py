2ad200d8aa18474eccb4a412e12a86dc48a8397a
yeram-lim <yeramyeye@gmail.com>
2022-03-10 12:06:59 +0900

    [pro] lv2 멀쩡한 사각형

--- 임예람/Programmers/2022/0310_lv2_멀쩡한사각형.py
+++ 임예람/Programmers/2022/0310_lv2_멀쩡한사각형.py
@@ -1,9 +1,11 @@
 W, H, result = 8, 12, 20
 
+from math import gcd
+
 def solution(w, h):
     total_square = w * h
-    answer = 1
-    return answer
+    greatest_common_divisor = gcd(w, h)
+    return total_square - (w + h - greatest_common_divisor)
 
 # 8:8 = 1:1 -> -w
 # 8:16 = 1:2 ->
